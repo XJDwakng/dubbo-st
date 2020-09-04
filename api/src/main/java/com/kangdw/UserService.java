004282b7e25fc4b537688c8bbb505ae57fbf1a03
kangdw <dingwu4567615>
2020-09-04 15:16:29 +0800

    2

---
+++ api/src/main/java/com/kangdw/UserService.java
@@ -0,0 +1,14 @@
+package com.kangdw;
+
+/**
+ * @author kangdw [kangdw@yinhai.com]
+ * @since 5.1
+ */
+public interface UserService {
+    /**
+     * 获取用户列表
+     *
+     * @return
+     */
+    public String getUserList();
+}
